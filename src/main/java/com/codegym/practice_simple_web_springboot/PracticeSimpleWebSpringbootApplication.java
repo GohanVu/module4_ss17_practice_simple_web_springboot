f1b638d2c3e15f70d38d45d07938800230e5f8b1
GohanVu <hoangvhdata@gmail.com>
2024-07-22 08:54:50 +0700

    update

---
+++ src/main/java/com/codegym/practice_simple_web_springboot/PracticeSimpleWebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_simple_web_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSimpleWebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSimpleWebSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codegym/practice_simple_web_springboot/PracticeSimpleWebSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_simple_web_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSimpleWebSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
